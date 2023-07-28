cf8e19c893e124ff6f6dbc240a96a1457838770a
p000059 <marcelo.acad@gmail.com>
2023-07-28 11:30:05 -0300

    create project ClassNull

---
+++ ClassNull/src/main/java/classnull/ClassNullApplication.java
@@ -0,0 +1,13 @@
+package classnull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassNullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassNullApplication.class, args);
+	}
+
+}
---
+++ ClassNull/src/test/java/classnull/ClassNullApplicationTests.java
@@ -0,0 +1,13 @@
+package classnull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassNullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
