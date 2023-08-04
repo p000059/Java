660dc10a7534eab906f49908d6e94166209ad651
p000059 <marcelo.acad@gmail.com>
2023-08-04 16:08:58 -0300

    create/update file Collections

--- Collections/src/main/java/collections/services/DriverCarService.java
+++ Collections/src/main/java/collections/services/DriverCarService.java
@@ -1,5 +1,19 @@
 package collections.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import collections.models.interfaces.validations.ICarListValidation;
+import collections.models.interfaces.validations.IDriverValidation;
+
 public class DriverCarService {
 
+	@Autowired
+	private ICarListValidation iCarListValidation;
+	
+	@Autowired
+	private IDriverValidation iDriverValidation;
+	
+	public  saveDriverCar() {
+		
+	}
 }
